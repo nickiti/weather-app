fc6ccc1956fe0f50c483513095bd2807fcfda9ca
nickiti <nickwin1999@yahoo.com>
2022-01-05 17:30:12 -0600

    first commit

---
+++ Desktop/weatherapp.js/main.js
@@ -0,0 +1,54 @@
+const api = {
+    key: "46ac210f23826ca5400cf2f3147f2b3f",
+    base : "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keydown', setQuery);
+
+function setQuery(evt) {
+    if(evt.keyCode == 13) {
+        getResults(searchbox.value); 
+    }
+}
+
+function getResults(query) {
+    fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+        .then(weather => {
+             return weather.json();
+        }).then(displayResults);
+}
+
+function displayResults(weather) {
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°</span>`;
+
+    let weatherel = document.querySelector('.current .weather');
+    weatherel.innerText = weather.weather[0].description;
+
+    let hilow = document.querySelector('.current .hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}° / ${Math.round(weather.main.temp_max)}°`;
+
+}
+
+function dateBuilder(d) {
+    const months = ["January","February","March","April","May","June",
+    "July","August","September","October","November","December",];
+    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday",
+    "Friday","Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day},
+         ${date} ${month} ${year}`;
+}
